a557055210feb5a6494177c40a85f72c85414a3e
melvin <melvin.mark@centrino.co.ke>
2022-03-26 08:52:24 +0300

    miscalleneous fixes

--- tracker/admin.py
+++ tracker/admin.py
@@ -4,29 +4,28 @@ from subcategory.models import SubCategory
 
 
 def compare_phoneplace():
-    dataset_phoneplace = Categories.objects.filter(website_id=2).values('link', 'idcategories')
+            dataset_phoneplace = Categories.objects.filter(website_id=2).values('link', 'idcategories')
 
-    website_prices = []
-    database_prices = ScrapPhoneplace().allPrices()
+            website_prices = []
+            database_prices = ScrapPhoneplace().allPrices()
 
-    for values in dataset_phoneplace:
-        website_prices.append(ScrapPhoneplace().trackprice(values['link']['link']))
+            for values in dataset_phoneplace:
+                website_prices.append(ScrapPhoneplace().trackprice(values['link']['link']))
 
-    for item_web in website_prices[0]:
-        res_db = next((item_database for item_database in database_prices if item_database['name'] == item_web['name']),
-                      False)
+            for item_web in website_prices[0]:
+                res_db = next((item_database for item_database in database_prices if item_database['name'] == item_web['name']),
+                              False)
 
-        if res_db:
+                if res_db:
+                    if res_db['current_price'] > item_web['price']:
+                        new_data_phoneplace().update_db(item_web['name'], item_web['price'])
+                else:
+                    ids = Categories.objects.filter(category_name__icontains=item_web['name'].split()[0]).values('idcategories')
+                    ins = SubCategory(
+                        category_id=ids,
+                        name=item_web['name'],
+                        current_price=item_web['price'],
+                        link=item_web['link']
 
-            if res_db['current_price'] > item_web['price']:
-                new_data_phoneplace().update_db(item_web['name'], item_web['price'])
-        else:
-            ids = Categories.objects.filter(category_name__icontains=item_web['name'].split()[0]).values('idcategories')
-            ins = SubCategory(
-                category_id=ids,
-                name=item_web['name'],
-                current_price=item_web['price'],
-                link=item_web['link']
-
-            )
-            ins.save()
+                    )
+                    ins.save()
